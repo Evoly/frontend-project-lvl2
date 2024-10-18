24b089ce1d80194920a10acb9fd649cee7d106ee
evoly <svetoch.sv@gmail.com>
2024-10-18 12:02:20 +0700

    refactoring

--- src/parse.js
+++ src/parse.js
@@ -1,35 +1,25 @@
-/* eslint no-nested-ternary:  */
-
 import fs from 'node:fs';
 import yaml from 'js-yaml';
 import path from 'node:path';
 
-const filePath = (filename) => path.resolve(process.cwd(), filename);
-
-const validateExt = (filename) => {
-  const ext = filename.endsWith('.json') ? 'json' : filename.endsWith('.yml') || filename.endsWith('.yaml') ? 'yml' : 'err';
-  return ext;
+const getParsedData = (data, ext) => {
+  switch (ext) {
+    case 'json':
+      return JSON.parse(data);
+    case 'yml':
+    case 'yaml':
+      return yaml.load(data);
+    default:
+      throw new Error(`Invalid extension - ${ext}`);
+  }
 };
 
 const data = (filename) => {
-  const pathToFile = filePath(filename);
-  const extension = validateExt(pathToFile);
-  if (extension === 'json') {
-    return JSON.parse(fs.readFileSync(pathToFile, { encoding: 'utf8', flag: 'r' }));
-  }
-  if (extension === 'yml') {
-    const ymlParse = yaml.load(fs.readFileSync(pathToFile, 'utf8'));
-    if (!Array.isArray(ymlParse)) return ymlParse;
+  const filePath = path.resolve(process.cwd(), filename);
+  const extension = path.extname(filePath).slice(1);
+  const filecontent = fs.readFileSync(filePath, { encoding: 'utf8' });
 
-    const result = [...ymlParse].map((el) => {
-      const key = Object.keys(el);
-      return [Object.keys(el), el[key]].flat();
-    });
-
-    return Object.fromEntries(result);
-  }
-
-  throw Error('wrong filename extension');
+  return getParsedData(filecontent, extension);
 };
 
 export default data;
